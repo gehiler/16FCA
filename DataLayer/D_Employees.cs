3b8ac40f108a7c4627c59da8b702a366fcf0f032
Gehiler Vargas E <gehiler@hotmail.com>
2018-10-14 16:56:43 -0400

    Initial Commit

---
+++ DataLayer/D_Employees.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataLayer
+{
+    public class D_Employees
+    {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["FCA"].ConnectionString);
+
+        public DataTable D_Listado()
+        {
+            SqlCommand cmd = new SqlCommand("sp_Listar", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
